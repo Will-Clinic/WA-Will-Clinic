d0dc11d81614f384cb81d488f4566886716171ee
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-02-26 19:14:00 -0800

    Renamed file, removed data annotations, removed redundant properties

---
+++ WillClinic/Models/Veteran.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WillClinic.Models
+{
+    public class Veteran
+    {
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZIP { get; set; }
+
+        public ICollection<VeteranChildrenModel> Children { get; set; }
+        public ICollection<VeteranIntakeModel> IntakeForms { get; set; }
+    }
+}
