a3962112e2c08325a2fc8bd7acc7d0c31147f15b
ZacharyJohnson <zaj6827@gmail.com>
2018-03-01 11:57:04 -0800

    Added removal functionality for Admin, added initialization for new property on Lawyer.

--- WillClinic/Controllers/AdminController.cs
+++ WillClinic/Controllers/AdminController.cs
@@ -27,25 +27,37 @@ namespace WillClinic.Controllers
             return View(results);            
         }
 
+        /// <summary>
+        /// This method is for verifying lawyers and updateing their status
+        /// </summary>
+        /// <param name="id">Attorney's Application User ID</param>
+        /// <returns>Attorney.isVerified = true; || Returns nothing if ID is not a match.</returns>
         public IActionResult VerifyLawyer(string id)
         {
             var attorney = _context.Lawyers.First(x => x.ApplicationUserId == id);
 
+            //If attorney ID is found and valid, change verified status
             if (attorney.ApplicationUserId == id)
             {
                 attorney.IsVerified = true;
                 _context.SaveChanges();
                 return RedirectToAction("Index");
             }
+            //If ID is not a match, rerender index page and make no changes
             else
             {
                 return RedirectToAction("Index");
             }
         }
 
+        /// <summary>
+        /// Method removes lawyer from queue if they cannot be verified or can no longer practice law.
+        /// </summary>
+        /// <param name="id">Lawyer Application ID</param>
+        /// <returns>If lawyer is found by ID, attorney.IsRejected is changed to true || no change is made if lawyer object is not valid.</returns>
         public IActionResult RemoveLawyer(string id)
         {
-            var attorney = _context.Lawyers.FirstOrDefault(x => x.ApplicationUserId == id);
+            var attorney = _context.Lawyers.First(x => x.ApplicationUserId == id);
             if (attorney != null)
             {
                 attorney.IsRejected = true;
