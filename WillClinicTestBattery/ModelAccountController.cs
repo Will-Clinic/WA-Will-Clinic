389432505d74bd52e1fdc10fa8b20b7a2ccc7a68
jeffxmartinez <jeffxmartinez@gmail.com>
2018-03-01 12:30:00 -0800

    Added registration links

--- WillClinicTestBattery/ModelAccountController.cs
+++ WillClinicTestBattery/ModelAccountController.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using WillClinic.Controllers;
+using Microsoft.AspNetCore.Identity;
+using WillClinic.Services;
+using Google.Apis.Logging;
+using WillClinic.Models;
 
 namespace WillClinicTestBattery
 {
-    class ModelAccountController
+    class ModelAccountController 
     {
     }
 }
