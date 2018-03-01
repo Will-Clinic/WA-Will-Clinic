ad400b13ca2af87686adaac705140d5645f263b6
ZacharyJohnson <zaj6827@gmail.com>
2018-03-01 11:00:00 -0800

    Adding property to lawyer, added functionaility to Admin to remove ineligible lawyers from validation list.

--- WillClinic/Controllers/AdminController.cs
+++ WillClinic/Controllers/AdminController.cs
@@ -18,14 +18,44 @@ namespace WillClinic.Controllers
             _context = ctx;
         }
         
-
         public IActionResult Index()
         {   
             // capturing the list of lawyers who are not yet verified
-            var results = _context.Lawyers.Include(x => x.ApplicationUser).Where(x => x.IsVerified == false).ToList();
+            var results = _context.Lawyers.Include(x => x.ApplicationUser).Where(x => x.IsVerified == false).Where(x => x.IsRejected != true).ToList();
 
             // Displaying above list to the View
             return View(results);            
+        }
+
+        public IActionResult VerifyLawyer(string id)
+        {
+            var attorney = _context.Lawyers.First(x => x.ApplicationUserId == id);
+
+            if (attorney.ApplicationUserId == id)
+            {
+                attorney.IsVerified = true;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public IActionResult RemoveLawyer(string id)
+        {
+            var attorney = _context.Lawyers.FirstOrDefault(x => x.ApplicationUserId == id);
+            if (attorney != null)
+            {
+                attorney.IsRejected = true;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
         }
 
         public IActionResult About()
