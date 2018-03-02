e5acbaa65cdb9e86fb6f908641b61a59d4e4bf7f
Luay <luayghazwan@yahoo.com>
2018-03-01 16:52:08 -0800

    Intake Form View Models modified to work accordingly with the Views and Model State Validation

--- WillClinic/Models/IntakeFormViewModels/IntakeFormViewModel2.cs
+++ WillClinic/Models/IntakeFormViewModels/IntakeFormViewModel2.cs
@@ -10,22 +10,18 @@ namespace WillClinic.Models.IntakeFormViewModels
     {
         [Required]
         [Display(Name = "Veteran Status")]
-        [Range(typeof(bool), "false", "true", ErrorMessage = "The field Veteran Status must be checked.")]
         public bool? VeteranStatus { get; set; }
 
         [Required]
         [Display(Name = "Proof Of Service")]
-        [Range(typeof(bool), "false", "true", ErrorMessage = "The field Proof Of Service must be checked.")]
         public bool? ProofOfService { get; set; }
 
         [Required]
         [Display(Name = "Resident Status")]
-        [Range(typeof(bool), "false", "true", ErrorMessage = "The field Resident Status must be checked.")]
         public bool? ResidentStatus { get; set; }
 
         [Required]
         [Display(Name = "Net Worth")]
-        [Range(typeof(bool), "false", "true", ErrorMessage = "The field Net Worth must be checked.")]
         public bool? NetWorth { get; set; }
     }
 }
