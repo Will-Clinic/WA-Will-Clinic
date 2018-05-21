4fa446daf7b8661540a4619aaafad9f9001bb8f7
philipwerner <philip.r.werner@gmail.com>
2018-05-21 15:05:07 -0700

    created razor pages for the will, poa, health directive

--- WillClinic/Models/VeteranIntakeForm.cs
+++ WillClinic/Models/VeteranIntakeForm.cs
@@ -17,12 +17,22 @@ namespace WillClinic.Models
         public bool? IsNotarized { get; set; }
 
         public bool? TermsAndConditions { get; set; }
+
         public string FullLegalName { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
         public bool? VeteranStatus { get; set; }
         public bool? ProofOfService { get; set; }
         public bool? ResidentStatus { get; set; }
+
+        public MaritalStatus MaritalStatus { get; set; }
+        public string FullNameSpouse { get; set; }
+        public bool? HaveChildren { get; set; }
+        public bool? UnderAgeChildren { get; set; }
+        public string MinorChildrenDifferentSpouse { get; set; }
+        public bool? CurrentlyPregnant { get; set; }
+        public string PrimaryGuardian { get; set; }
+
         public bool? NetWorth { get; set; }
         public int BankAccountAssets { get; set; }
         public int RealEstateAssets { get; set; }
@@ -35,12 +45,7 @@ namespace WillClinic.Models
         public int OtherAssetsOrMoney { get; set; }
         public int HouseHoldSize { get; set; }
         public int MonthlyIncome { get; set; }
-        public MaritalStatus MaritalStatus { get; set; }
-        public string FullNameSpouse { get; set; }
-        public bool? HaveChildren { get; set; }
-        public bool? UnderAgeChildren { get; set; }
-        public string MinorChildrenDifferentSpouse { get; set; }
-        public bool? CurrentlyPregnant { get; set; }
+
         public bool? SpecificBequest { get; set; }
         public string BequestInfromation { get; set; }
         public InheritEstate InheritEstate { get; set; }
@@ -49,13 +54,13 @@ namespace WillClinic.Models
         public string RemainderBeneficiarySpecific { get; set; }
         public bool? DisinheritSomeone { get; set; }
         public string DisinheritDescription { get; set; }
-        public string PrimaryGuardian { get; set; }
         public string AlternateGuardian { get; set; }
         public string PersonalRepresentative { get; set; }
         public string AlternateRepresentative { get; set; }
         public bool? RequestPowerOfAttorney { get; set; }
         public string PrimaryAttorney { get; set; }
         public string AlternateAttorney { get; set; }
+
         public bool? HealthCareDirective { get; set; }
         public bool? HydrationDirective { get; set; }
         public bool? NutritionDirective { get; set; }
---
+++ WillClinic/Pages/PersonalInfo.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WillClinic.Data;
+
+namespace WillClinic.Pages
+{
+    public class PersonalInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
