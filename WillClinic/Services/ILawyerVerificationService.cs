a41bd028474ea19a9a2750d85fe7121622505e64
wtnerb <wtnerb@gmail.com>
2018-05-22 13:11:31 -0700

    Adding lawyer verification files, Josh's adjustments

---
+++ WillClinic/Services/ILawyerVerificationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WillClinic.Services
+{
+    interface ILawyerVerificationService
+    {
+        Task<bool> IsValidLawyer(string name, string number, string email);
+    }
+}
