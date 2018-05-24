b757ea1dabf35dac334962eeb11943fff6a300ea
wtnerb <wtnerb@gmail.com>
2018-05-24 12:28:40 -0700

    All but sending email

--- WillClinic/Pages/Accounts/Register.cshtml.cs
+++ WillClinic/Pages/Accounts/Register.cshtml.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using WillClinic.Data;
 using WillClinic.Models;
+using WillClinic.Services;
 
 namespace WillClinic.Pages.Accounts
 {
     public class RegisterModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+        private EmailSender _emailSender { get; set; }
+        private UserManager<ApplicationUser> _userManager { get; set; }
 
         [BindProperty]
-        public ApplicationUser ApplicationUser { get; set; }
+        public ApplicationUser NewUser { get; set; }
         /// <summary>
         /// Gathers the type of user that is registered to be used to determine
         /// which verification e-mail to send out. Should match the values found
@@ -27,9 +31,11 @@ namespace WillClinic.Pages.Accounts
         public string SelectedUserType { get; set; }
         public SelectList UserTypes { get; set; }
 
-        public RegisterModel(ApplicationDbContext context)
+        public RegisterModel(ApplicationDbContext context, EmailSender emailSender, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _emailSender = emailSender;
+            _userManager = userManager;
         }
 
         public IActionResult OnGet()
@@ -45,11 +51,16 @@ namespace WillClinic.Pages.Accounts
                 return Page();
             }
 
-            _context.ApplicationUser.Add(ApplicationUser);
+            _context.ApplicationUser.Add(NewUser);
             await _context.SaveChangesAsync();
+            
+            string code = await _userManager.GenerateEmailConfirmationTokenAsync(NewUser);
 
-            // TODO(taylorjoshuaw): Add email service call to send either a veteran or lawyer verification link
-            //                      to the user
+            //TODO refactor this to use pages
+            string link = Url.Action("Verify", "Acount",
+                new { email = NewUser.Email, code, userType = SelectedUserType }, "https", HttpContext.Request.Host.Value);
+
+            await _emailSender.SendEmailConfirmationAsync(NewUser.Email, link);
 
             return RedirectToPage("./EmailVerificationSent");
         }
